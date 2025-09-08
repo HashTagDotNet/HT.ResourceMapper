93be816b298c399167f85c18745b492e439a88fa
Steve Powell <spowell@descartes.com>
2025-09-08 12:21:18 -0400

    --

--- Database/HTResourceMapper/StoredProcedures/Resource_GetItems.sql
+++ Database/HTResourceMapper/StoredProcedures/Resource_GetItems.sql
@@ -39,14 +39,14 @@ BEGIN
     BEGIN
         SET @whereClause = '
         WHERE (
-            r.ResourceKey LIKE @SafeSearchParam ESCAPE '']''
-            OR r.ResourceName LIKE @SafeSearchParam ESCAPE '']''
-            OR r.Description LIKE @SafeSearchParam ESCAPE '']''
+            r.ResourceKey LIKE @SafeSearchFor ESCAPE '']''
+            OR r.ResourceName LIKE @SafeSearchFor ESCAPE '']''
+            OR r.Description LIKE @SafeSearchFor ESCAPE '']''
             OR EXISTS (
                 SELECT 1 FROM [HTResourceMapper].[ResourceTag] rt2
                 INNER JOIN [HTResourceMapper].[TagDefinition] td2 ON rt2.TagDefinitionId = td2.TagDefinitionId
                 WHERE rt2.ResourceId = r.ResourceId
-                AND (rt2.TagValue LIKE @SafeSearchParam ESCAPE '']'' OR td2.TagDefinitionKey LIKE @SafeSearchParam ESCAPE '']'')
+                AND (rt2.TagValue LIKE @SafeSearchFor ESCAPE '']'' OR td2.TagDefinitionKey LIKE @SafeSearchFor ESCAPE '']'')
             )
         )';
     END
@@ -78,7 +78,7 @@ BEGIN
     IF @SafeSearchFor IS NOT NULL
     BEGIN
         EXEC sp_executesql @sql, 
-            N'@SafeSearchParam NVARCHAR(255), @TotalRecords INT OUTPUT', 
+            N'@SafeSearchFor NVARCHAR(255), @TotalRecords INT OUTPUT', 
             @SafeSearchFor, @TotalRecords OUTPUT;
     END
     ELSE
@@ -90,7 +90,7 @@ BEGIN
     
     -- Get paged resources with all their tags in single resultset
     SET @sql = '
-    WITH PagedResources AS (
+    ;WITH PagedResources AS (
         SELECT DISTINCT r.ResourceId, r.ResourceUid, r.ResourceName, rt_type.TypeName as ResourceType,
                r.Description, COALESCE(r.UpdatedOn, r.CreatedOn) as LastUpdatedOn
         FROM [HTResourceMapper].[Resource] r WITH(NOLOCK)
@@ -112,9 +112,9 @@ BEGIN
             tct.TagCode as ContentType,
             rt.TagValue,
             CASE 
-                WHEN @SafeSearchParam IS NOT NULL AND (
-                    rt.TagValue LIKE @SafeSearchParam ESCAPE '']''
-                    OR td.TagDefinitionKey LIKE @SafeSearchParam ESCAPE '']'')
+                WHEN @SafeSearchFor IS NOT NULL AND (
+                    rt.TagValue LIKE @SafeSearchFor ESCAPE '']''
+                    OR td.TagDefinitionKey LIKE @SafeSearchFor ESCAPE '']'')
                 THEN 0  -- Matching tags get priority 0 (highest)
                 ELSE 1  -- Non-matching tags get priority 1
             END as Priority,
@@ -122,9 +122,9 @@ BEGIN
                 PARTITION BY pr.ResourceUid 
                 ORDER BY 
                     CASE 
-                        WHEN @SafeSearchParam IS NOT NULL AND (
-                            rt.TagValue LIKE @SafeSearchParam ESCAPE '']''
-                            OR td.TagDefinitionKey LIKE @SafeSearchParam ESCAPE '']'')
+                        WHEN @SafeSearchFor IS NOT NULL AND (
+                            rt.TagValue LIKE @SafeSearchFor ESCAPE '']''
+                            OR td.TagDefinitionKey LIKE @SafeSearchFor ESCAPE '']'')
                         THEN 0 
                         ELSE 1 
                     END,
@@ -154,7 +154,7 @@ BEGIN
     IF @SafeSearchFor IS NOT NULL
     BEGIN
         EXEC sp_executesql @sql, 
-            N'@SafeSearchParam NVARCHAR(255), @Skip INT, @Take INT', 
+            N'@SafeSearchFor NVARCHAR(255), @Skip INT, @Take INT', 
             @SafeSearchFor, @Skip, @Take;
     END
     ELSE
