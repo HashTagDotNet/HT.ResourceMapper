40edef0ec6296a93c191ae9ba22b9c6945b4d981
Steve Powell <spowell@descartes.com>
2025-09-20 15:47:15 -0400

    --

---
+++ Modules/Common/ResourceMapper.Common.Shared/Editor/Contracts/OpenEditorContract.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResourceMapper.Common.Shared.Editor.Contracts
+{
+    internal class OpenEditorContract
+    {
+    }
+}
